4c3876d886ca968cf5b3b49e81b862e1f336809e
Navil <Navil@DESKTOP-6CJ4T69>
2025-08-03 08:03:05 +0100

    this project contains  the sample spring boot

---
+++ SpringbootSamlpe/src/main/java/com/tnsif/SpringbootSamlpe/SpringbootSamlpeApplication.java
@@ -0,0 +1,14 @@
+package com.tnsif.SpringbootSamlpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSamlpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSamlpeApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
---
+++ SpringbootSamlpe/src/test/java/com/tnsif/SpringbootSamlpe/SpringbootSamlpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.SpringbootSamlpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSamlpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
